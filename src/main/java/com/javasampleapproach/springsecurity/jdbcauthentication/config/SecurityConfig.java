b891513c8607c7f821157e9430bf5b176dfad17c
Rangareddy <rangareddyjava9@gmal.com>
2019-04-10 22:46:25 +0530

    commit

--- src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/SecurityConfig.java
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
+
 	@Autowired
 	DataSource dataSource;
  
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,14 @@
-CREATE TABLE users(
+DROP TABLE if EXISTS user_roles;
+DROP TABLE if EXISTS users;
+
+
+CREATE TABLE if not EXISTS users(
    username varchar(20) NOT NULL,
    password varchar(20) NOT NULL,
    enabled boolean NOT NULL DEFAULT FALSE,
    primary key(username)
 );
-create table user_roles (
+create table if not EXISTS user_roles (
   user_role_id SERIAL PRIMARY KEY,
   username varchar(20) NOT NULL,
   role varchar(20) NOT NULL,
