cf76490cc3b1125804bc05efad0b0c8e50e0d022
Rangareddy <rangareddyjava9@gmal.com>
2019-04-11 22:20:20 +0530

    commit

--- src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/AuthenticationSuccessEventListener.java
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/AuthenticationSuccessEventListener.java
@@ -2,6 +2,7 @@ package com.javasampleapproach.springsecurity.jdbcauthentication.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
@@ -17,13 +18,24 @@ public class AuthenticationSuccessEventListener
     @Autowired
     private HttpServletRequest request;
 
-    public void onApplicationEvent(AuthenticationSuccessEvent e) {
+    public void onApplicationEvent(AuthenticationSuccessEvent e) {String ip = getClientIP();
+        if (loginAttemptService.isBlocked(ip)) {
+            throw new AuthenticationCredentialsNotFoundException("blocked");
+        }
+
         final String xfHeader = request.getHeader("X-Forwarded-For");
         if (xfHeader == null) {
             loginAttemptService.loginSucceeded(request.getRemoteAddr());
         } else {
             loginAttemptService.loginSucceeded(xfHeader.split(",")[0]);
         }
+    }
+    private String getClientIP() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null){
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
     }
 }
 
